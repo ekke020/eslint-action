c45096e3860cf556a46998f0c704a4c92117543b
Johan Ekman <johan.ekman@pricer.com>
2023-01-18 16:29:34 +0100

    Trying now

--- action.js
+++ action.js
@@ -41,7 +41,7 @@ const main = async () => {
 
   const octokit = github.getOctokit(myToken);
 
-  const [context] = github;
+  const context = github.context;
 
   const newIssue = await octokit.rest.issues.createComment({
     ...context.repo,
