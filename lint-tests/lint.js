ea9326ce19870ea3963f76a4af26a93be68ee454
Johan Ekman <johan.ekman@pricer.com>
2023-01-24 15:02:15 +0100

    Bad

--- lint-tests/lint.js
+++ lint-tests/lint.js
@@ -1,11 +1,11 @@
-    const bad  = "wrong stuff"
+  const bad  = "wrong stuff"
 
-    const badArray = ["a", 'b', 'c', 'd'];
+  const badArray = ["a", 'b', 'c', 'd'];
 
-    export const test = () => {
-      const a = badArray[0];
-    };
+  export const test = () => {
+    const a = badArray[0];
+  };
 
-    const badFunction = () => {
-      console.log('This is not indented correctly');
-    }
+  const badFunction = () => {
+    console.log('This is not indented correctly');
+  }
