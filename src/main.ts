e523886dcf0012f48a82c2adac56e3d41ca13cc9
Johan Ekman <johan.ekman@pricer.com>
2023-01-25 22:51:44 +0100

    Added some logic

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ import {
 
 const AUTOFIX = core.getInput('auto_fix') === 'true';
 const TOKEN = core.getInput('token');
+const COMMENT = core.getInput('comment') === 'true';
+const COMMENT_LIMIT = Number(core.getInput('comment_limit')) || 3;
+
 const octokit = github.getOctokit(TOKEN);
 
 const id = github.context.payload.pull_request!.number;
@@ -27,17 +30,20 @@ const createReviewMessage = (messages: Set<string>): string => [...messages].red
   '',
 ).trimEnd();
 
-const createReviewComment = async (information: ErrorInformation, path: string) => {
-  await octokit.rest.pulls.createReviewComment({
+const createReviewComment = async (
+  information: ErrorInformation,
+): Promise<boolean> => {
+  const res = await octokit.rest.pulls.createReviewComment({
     owner,
     repo,
     pull_number: id,
     body: createReviewMessage(information.messages),
     commit_id: commitId,
-    path: getRelativePath(path),
+    path: getRelativePath(information.filePath),
     line: information.endLine ?? information.line,
     start_line: information.endLine ? information.line : undefined,
   });
+  return res.status === 201;
 };
 
 const createComment = async (message: string) => {
@@ -64,23 +70,30 @@ const presentAllErrors = async (results: ESLint.LintResult[]) => {
   await createFormattedComment(formatted, 'There are errors outside of the commit\n');
 };
 
+const postComments = async (results: ESLint.LintResult[]) => {
+  const messages = results.flatMap((result) => GroupMessages(result.messages, result.filePath));
+  let count = 0;
+  let isError = false;
+  messages.some(async (message) => {
+    isError = await createReviewComment(message) ? isError : true;
+    return ++count > COMMENT_LIMIT;
+  });
+  if (isError) {
+    await presentAllErrors(results);
+  }
+};
+
 const main = async () => {
-  console.log(github.context);
   let results = await lint();
   if (AUTOFIX) {
-    // TODO: Add automatic git commit after code fix.
     fixCodeErrors(results);
     results = filterOutFixable(results);
   }
-  try {
-    for await (const result of results) {
-      const groupedMessages = GroupMessages(result.messages);
-      for await (const message of groupedMessages) {
-        await createReviewComment(message, result.filePath);
-      }
-    }
-  } catch (err) {
-    await presentAllErrors(results);
+  if (COMMENT) {
+    await postComments(results);
+  } else {
+    const formatted = await formatedResult(results);
+    await createFormattedComment(formatted);
   }
 };
 
