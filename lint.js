c7eb593ba3fc77b783c13fdf1a31d0eed11eefdc
bot <github@notreply.com>
2023-01-25 14:05:14 +0100

    from API

---
+++ lint.js
@@ -0,0 +1,11 @@
+const bad  = "wrong stuff"
+
+const badArray = ["a", 'b', 'c', 'd'];
+
+export const test = () => {
+  const a = badArray[0];
+};
+
+const badFunction = () => {
+  console.log('This is not indented correctly');
+}
