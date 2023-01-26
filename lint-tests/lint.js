af6417822b6e6d3dc716a00e2a5b75f079dd4fde
Johan Ekman <johan.ekman@pricer.com>
2023-01-26 16:34:33 +0100

    Fixed stuff

--- lint-tests/lint.js
+++ lint-tests/lint.js
@@ -1,11 +1,14 @@
-const bad  = "wrong stuff"
+const bad = 'wrong stuff';
 
-const badArray = ["a", 'b', 'c', 'd'];
+const badArray = ['a', 'b', 'c', 'd'];
 
 export const test = () => {
   const a = badArray[0];
+  console.log(a, bad);
 };
 
 const badFunction = () => {
   console.log('This is not indented correctly');
-}
+};
+badFunction();
+export default test;
