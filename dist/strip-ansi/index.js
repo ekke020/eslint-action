52aa35dc9f8b9593d667c5f34b8b8d01e5578ea7
ekke020 <ekke020@users.noreply.github.com>
2023-01-31 08:00:30 +0000

    New build release

--- dist/strip-ansi/index.js
+++ dist/strip-ansi/index.js
@@ -1,4 +1,4 @@
 'use strict';
-const ansiRegex = require('ansi-regex');
+const ansiRegex = require('../ansi-regex');
 
 module.exports = string => typeof string === 'string' ? string.replace(ansiRegex(), '') : string;
